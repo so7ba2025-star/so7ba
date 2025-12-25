9de47660718c342712e3f2dde87a686c111b9948
so7ba2025-star <so7ba2025@gmail.com>
2025-12-25 14:12:41 +0200

    Deploy web (Flutter build)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1435897298"
+    serviceWorkerVersion: "4230526584"
   }
 });
